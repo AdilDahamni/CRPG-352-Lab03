a7aef8e172b87fa74a12a9bc10ee790dc5942f61
dBsystems <dBsystems@PC>
2021-10-03 23:23:14 -0600

    New Project Creation Created a new Java Application for HelloWorld.

---
+++ src/java/servlet/AgeCalculatorServlet.java
@@ -0,0 +1,49 @@
+package servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AgeCalculatorServlet extends HttpServlet {
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // this will have the servlet call upon a JSP to be loaded by the client's browser
+        getServletContext().getRequestDispatcher("/WEB-INF/agecalculator.jsp").forward(request,response);
+        // stop the code call
+        return;
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+       
+        String Age;
+        
+    Age = request.getParameter("age");
+    request.setAttribute("age", Age);
+    
+        if(Age == null || Age.equals("")){
+         request.setAttribute("message", "You must give your current age.");
+            
+            getServletContext().getRequestDispatcher("/WEB-INF/agecalculator.jsp").forward(request,response);
+            return;
+    }
+        
+            
+        }
+        
+        
+      
+        
+
+        // The request object will be passed through to the JSP by the forward() method.
+       // getServletContext().getRequestDispatcher("/WEB-INF/sayHello.jsp").forward(request,response);
+       // return;
+        
+    }
+
+
---
+++ web/WEB-INF/agecalculator.jsp
@@ -0,0 +1,25 @@
+<%-- 
+    Document   : agecalculator
+    Created on : 3-Oct-2021, 10:34:45 PM
+    Author     : dBsystems
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Age Calculator</title>
+    </head>
+    <body>
+         <form method="POST" action="hello">
+             <h1>Age Calculator</h1>
+            <label>Enter your age:</label>
+            <input type="text" name="age" value="${Age}">
+     
+            <br><br>
+            <input type="submit" value="Age next birthday">
+        </form>
+        <p>${message}</p>
+    </body>
+</html>
